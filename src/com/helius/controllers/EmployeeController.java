2e0ae542d87717731b1fb6ec5790dba24f477686
vinay <vinay.er@helius-tech.com>
2020-09-15 14:10:42 +0530

    removing unnecessary code temp

--- src/com/helius/controllers/EmployeeController.java
+++ src/com/helius/controllers/EmployeeController.java
@@ -75,8 +75,6 @@ public class EmployeeController {
 	@Autowired
 	private EmailService emailService;
 	
-	private final String imagedir = "C:" + File.separator + "Users" + File.separator + "HELIUS" + File.separator
-			+ "Documents" + File.separator + "employee_photo";
 	private final String fileLocation = "C:" + File.separator + "Users" + File.separator + "HELIUS" + File.separator
 			+ "Documents";
 	private final String noPhoto = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgICRAQCwgICA4JFwoNCwoKBhsICQgWIB0iIiAdHx8kKCgsGCYlJx8fITEhJSkrLi4uIx8zODMsNygtLisBCgoKBQ0KEgUPDisZExkrKysrKysrKysrKysrLS0rKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIAIAAgAMBIgACEQEDEQH/xAAbAAACAwEBAQAAAAAAAAAAAAAAAQMEBQYCB//EADYQAAIBAwIDBQUIAgMBAAAAAAECAAMREgQhBSIxE0FRcbEyQmGRoRQzcoHB0eHwBlJDRGIj/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAH/xAAVEQEBAAAAAAAAAAAAAAAAAAAAAf/aAAwDAQACEQMRAD8A+xMSxuTe8UISghCEAhCEAhCEAhCEAiJgTETAIExExEwAmNWKm4NrTzETAmhCEAhCECPUMUpOQbFVcg+BAM5XSca1q1UNWuzpdc1KDcd/QTqdV9y/4avoZwwEsSux4xXqUdG70nwYYYuFDdTMz/H9fq9TXYVazVAFyAKBbG/wER1Pb8HYE3algjeQIsflIf8AGrLqHPgjE/OBd43xdtM3Z0cc/fcrl2fwA8ZhjiXEAcvtVb825Pl0nk5anUbnes2/5mddV0tJ9OaWKhLYqMengfOBS4NxU6vkqhVqLuCOVag/eal5xeiqGhqEbpgyg+V7GdmZAXiMDEYUExXgTETCLEIQhRCEIEep+5f8NX0M5Hh1EVtQqHo+a+VwZ12p+5f8L+hnL8IFtXT829DCVArPRFSm1xnsw+INx+vzlvgQPaVPijyTj2mwr5gbVdz5jr+kf+Pi1dvw/rKM/QADU0vgyX+c68GcnqKLaauR07M3U/DqDNurxWh2BYNzsNqfvAwOdcXqG3ezW+c7Imctw6ia2oUWvY5MfgN51BMAJivAmImQBMV4GImBahCEKcUxOPOy1Vszryt0bHvl+s5XRE3Nwi7+9ewgWaql0YdMgy38xMrR8IqUKyuaqN2e9gh5tpn6Ks610JqORdQQXPftJ+MVX+0kBnXEKLBjCNXiGlGqpY3CkYsrFekr8O4e2lqEmor5DGyqfGeuDuW0+5LFS4uWyhxeoU05sSC5UCzY/EwPWv0NPVC9yjLsHC93gZmjg1e/3tK3jv6Wnrg1Zu2IZmbMbXbLcTQ4hqTQpXFsm5Vv7vxgGj0dPSrZbsze059pv4lgmYCabU6hS9y1st2fmPlL3Ca9VwQ4dwN1cr9LwNE38DEZz5FWpWKqz3YtYZlZp8Oo1aKt2hvkVI58trQLhiJiJigXYiYExQrF49vVX8Lest6w20HmtIekqcc3qL+FvWWOIG2iX/12I+n8SoyAhFMMP9mHyAIk1e1evUPWwZvlYSQU8tCT/o+X0Aj0FO9OsfBWUQLPBD/82Hgb/SRcbe7IvhkT+ew/WPgxsXHjifWQa5mqak2GRXFVAXK9oAR9m1g7guHyIA/eTcbJJQeGZ9JV1T1aj5VFKE7DkxvaWteDVoU3622P5j9xAlpVhptIhxL32sGx63j02vWtUCimyX6HIbSPTVqDacLVKns/db3vC0g4amVe9rBcj5XgQCoaNcsADiW2PszV0dc16dyFWxtYTNolV1N2IABa5PszVpVKb+wVa3XFYHuBMRMCZBcMV4rwvCqus0S6lgTUZMdrBQ0eo0q1qSoXZQmO4Ub2FpX4uxIRR72TW8pXqA1NGpvvSOJPw/toReTSKtA08mIfLmK80KGlSijLmW7TqSoXutKmtqmrSpgHd8SfPp6yPiN8wq/8Sr/fSBc0ujGnYkVGe+1ioWKlolp1c+0ZzzGxUdTItfU7TTIb+1ifoZ4/7VPyT0MC3q9OuoAuxXHI3CwpUVSlhfMc3VfGeda+NBu6/L85X4YSCynuxNv7+UAfhqE7VGUeBXKWaFFKK2Xv3JPtGZQAsxzKsp5QG9reWKrFqlEnqQpPzgSPw9CSe0cX39kSbTUFoA2YvlvvK/ERd03tfIfUSxpqa01srZ33vAliMIQLcCYExEwMvV1SdXsjVOz2xHl/MWgGaVKZBGW4B7j0/aX0o00csFIZ+pygtGmtQsBZm6nKBl6FS9ZQelPI/U/qYPULVKhwapnkoIXp8fpNJKNKmxKrYv1OUKVGnSviLZbnmgZjPlpAP9Gt+VjJXYLqKZJCgBLk+Rlr7JQAtibHcjI9YVNNSc7qTbFRzdwgV+I1AyKFOWe+3vd0joVCNSCUZM+XFvL+Jc+zUhbl+79nm6d8KlGm7AkXK9DlAywEKOSbMCuPN13kzuS1IsbbKST5mW/stC98L9+7R1aFOpbIXtsLNjtAq65kdksQRzAkeYlrTimqWQ5DmN8st55OlpEWx6ZW5p7pU1piyi19/age4QhAtTyTPTqVNiLWnm8AJiMRhAIiYiYoDvETCImAExQigEIQgEIQgEIT0iM7WUEkwP/Z";
@@ -91,160 +89,6 @@ public class EmployeeController {
 	 */
 	public EmployeeController() {
 		// TODO Auto-generated constructor stub
-	}
-	
-	public static void readExcel(int bankidindex, int jobtypeindex, int grandtotalindex) throws IOException {
-		HashMap<String, ArrayList<Row>> uniqueJobtypHrs = new HashMap<String, ArrayList<Row>>();
-		HashMap<String, String> nameID = new HashMap<String, String>();
-		FileInputStream file = new FileInputStream(new File("C:/Users/HELIUS/Documents/RTSNOV142019.xlsx"));
-		Workbook workbook = new XSSFWorkbook(file);
-		Sheet sheet = workbook.getSheetAt(0);
-		int i = 0;
-		String uniqueId = null;
-		String jobtype = null;
-		String workinghours = null;
-		String prevUniqueId = null;
-		int count = 0;
-		int hearderindex = 0;
-		int totalindex = 0;
-		for (Row row : sheet) {
-			if (row.getCell(bankidindex) != null) {
-				if ("Inscope 1bankID".equalsIgnoreCase(row.getCell(bankidindex).toString())) {
-					totalindex = row.getLastCellNum();
-					break;
-				}
-			}
-			hearderindex++;
-		}
-		for (Row row : sheet) {
-			if (i > hearderindex) {
-				if (row.getCell(bankidindex) == null || row.getCell(bankidindex).toString().isEmpty()) {
-					uniqueId = prevUniqueId;
-					if (uniqueJobtypHrs.containsKey(uniqueId)
-							&& (row.getCell(jobtypeindex) != null && !row.getCell(jobtypeindex).toString().isEmpty())) {
-						ArrayList<Row> rowdata = uniqueJobtypHrs.get(uniqueId);
-						rowdata.add(row);
-						uniqueJobtypHrs.put(uniqueId, rowdata);
-					}
-				} else {
-					uniqueId = row.getCell(bankidindex).toString();
-					
-					 // if (uniqueId.equals("Grand Total")) { continue; }
-					 
-					if (row.getCell(jobtypeindex) == null || row.getCell(jobtypeindex).toString().isEmpty()) {
-						continue;
-					}
-					prevUniqueId = uniqueId;
-					ArrayList<Row> rowdata = new ArrayList<Row>();
-					rowdata.add(row);
-					nameID.put(uniqueId, row.getCell(1).toString());
-					uniqueJobtypHrs.put(uniqueId, rowdata);
-				}
-			}
-			i++;
-		}
-		System.out.println("Number of employees " + (uniqueJobtypHrs.size()));
-		String uniquegrandtotal = calculateGrandHours(grandtotalindex, uniqueJobtypHrs, nameID);
-		if (workbook != null) {
-			workbook.close();
-		}
-	}
-	
-	public static Set<String> readNoClock() throws IOException {
-		Set<String> resourceList = new HashSet<String>();
-		FileInputStream file = new FileInputStream(new File("C:/Users/HELIUS/Documents/RTS-Resources.xlsx"));
-		Workbook workbook = new XSSFWorkbook(file);
-		Sheet sheet = workbook.getSheetAt(0);
-		int i = 0;
-		String rtsResourceId = null;
-		int count = 0;
-		int hearderindex = 0;
-
-		for (Row row : sheet) {
-			if (row.getCell(0) != null) {
-				if ("Inscope 1bankID".equalsIgnoreCase(row.getCell(0).toString())) {
-					break;
-				}
-			}
-			hearderindex++;
-		}
-		for (Row row : sheet) {
-			if (i > hearderindex) {
-				rtsResourceId = row.getCell(0).toString();
-				resourceList.add(rtsResourceId);
-			}
-			i++; 
-		}
-		if (workbook != null) {
-			workbook.close();
-		}
-		return resourceList;
-	}
-	
-	public static String calculateGrandHours(int totalindex, HashMap<String, ArrayList<Row>> uniqueJobtypHrs,HashMap<String, String> nameID) {
-		float validhrs = 168;
-		float sum = 0;
-		Integer i = 0;
-		String hoursList = null;
-		// List<String> resourceList = null;
-		System.out.println("below are the employee's onebankId which did not match the total valid hrs for the month - Total valid hrs =  " + validhrs );
-		System.out.println("----------------------------");
-		//Set<String> resourceList = new HashSet<String>();
-		String csvId = null;
-		String csvName = null;
-		Set<String[]> clockres = new HashSet<String[]>();
-		/*try {
-			resourceList = readNoClock();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}*/
-		for (Map.Entry<String, ArrayList<Row>> entry : uniqueJobtypHrs.entrySet()) {
-			String key = entry.getKey();
-			ArrayList<Row> rows = entry.getValue();
-			
-			 // for(Row values : rows){ 
-			  // System.out.println("key : " + key +" value : " + values); hoursList =
-			 // values.getCell(totalindex).toString(); }
-		
-			Iterator<Row> iter = rows.iterator();
-			while (iter.hasNext()) {
-				Row row = iter.next();
-				hoursList = row.getCell(totalindex).toString();
-				float hrs = Float.parseFloat(hoursList);
-				sum = sum + hrs;
-			}
-			if (sum < validhrs || sum > validhrs) {
-				csvName = nameID.get(key).toString();
-			//	System.out.println("1bankid=" + key + "  name=" + csvName + " RTS hours " + sum + " did not match the total valid hrs for the month");
-				System.out.println("1bankid=" + key + "  name=" + csvName + " RTS hours " + sum);
-			}
-			if (sum == validhrs) {
-				if (nameID.containsKey(key)) {
-					csvName = nameID.get(key).toString();
-					String[] idname = new String[] { key, csvName };
-					clockres.add(idname);
-				}
-			}
-			sum = 0;
-			//resourceList.remove(key);
-		}
-		//System.out.println("Matches the total valid hrs for the month" + clockres);
-		try {
-			// We have to create CSVPrinter class object and path will be stored
-			// in base project folder
-			Writer writer = Files.newBufferedWriter(Paths.get("correctclockers.csv"));
-			// CSVPrinter csvPrinter = new CSVPrinter(writer,
-			// CSVFormat.DEFAULT.withHeader("onebankID"));
-			CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.EXCEL.withHeader("onebankID", "onebankName"));
-			csvPrinter.printRecords(clockres);
-			csvPrinter.flush();
-			csvPrinter.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	//	System.out.println("NO Clock One bankId" + resourceList);
-		return "";
 	}
 	
 	@CrossOrigin
@@ -304,63 +148,6 @@ public class EmployeeController {
 	}
 	
 	@CrossOrigin
-	@RequestMapping(value = "getAllOffers", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
-	public @ResponseBody String getAllOffers() {
-		EmployeeManager employeemanager = (EmployeeManager) context.getBean("employeeManager");
-		String offerdetailsByStatus = employeemanager.getAllOffers();
-		return offerdetailsByStatus;
-	}
-	
-	@CrossOrigin
-	@RequestMapping(value = "getNewEmployeesOngivenDate", method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<String> getNewEmployeesOngivenDate(@RequestParam String selectedDate) {	
-		ResponseEntity<String> newEmpJoinee = null;
-		try{
-		List<Object> newJoinee = employeemanager.getAllNewEmployees(selectedDate);
-		if (newJoinee != null && !newJoinee.isEmpty() ) {
-			ObjectMapper om = new ObjectMapper();
-			String newJoinee1 = om.writeValueAsString(newJoinee);
-			newEmpJoinee = new ResponseEntity<String>(newJoinee1, HttpStatus.OK);
-		} else {
-			newEmpJoinee = new ResponseEntity<String>(HttpStatus.NO_CONTENT);
-		}}catch (Exception e) {
-			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (Throwable e) {
-			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		return newEmpJoinee;
-	}
-	
-	@CrossOrigin
-	@RequestMapping(value = "getResignEmployeesOngivenDate", method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<String> getResignEmployeesOngivenDate(@RequestParam String selectedDate,@RequestParam String fromdate,@RequestParam String todate) {	
-		ResponseEntity<String> resignEmployees = null;
-		try{
-		List<Object> resignEmp = employeemanager.getAllResignEmployees(selectedDate,fromdate,todate);
-		if (resignEmp != null && !resignEmp.isEmpty() ) {
-			ObjectMapper om = new ObjectMapper();
-			String resignEmp1 = om.writeValueAsString(resignEmp);
-			resignEmployees = new ResponseEntity<String>(resignEmp1, HttpStatus.OK);
-		} else {
-			resignEmployees = new ResponseEntity<String>(HttpStatus.NO_CONTENT);
-		}}catch (Exception e) {
-			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (Throwable e) {
-			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		return resignEmployees;
-	}
-	
-	
-	@CrossOrigin
-	@RequestMapping(value = "getOpenOffers", method = RequestMethod.GET, produces = "application/json")
-	public @ResponseBody String getOpenOffers() {
-		EmployeeManager employeemanager = (EmployeeManager) context.getBean("employeeManager");
-		String openoffer = employeemanager.getOpenOffers();
-		return openoffer;
-	}
-	
-	@CrossOrigin
 	@RequestMapping(value = "getOfferByID", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
 	public @ResponseBody String getOfferbyID(@RequestParam String offerid) {
 		EmployeeManager employeemanager = (EmployeeManager) context.getBean("employeeManager");
@@ -398,25 +185,7 @@ public class EmployeeController {
 			String	totaljson = "{\"employeeOffer\":" + empofferjson + "}";
 			return empofferjson;
 	}
-	
-	/* picklist for employee assignment entity */
-	@CrossOrigin
-	@RequestMapping(value = "employee/getPicklist", method = RequestMethod.GET, produces = "application/json")
-	public @ResponseBody String getHM() {
-		EmployeeManager employeemanager = (EmployeeManager) context.getBean("employeeManager");
-		String HM = employeemanager.masterpicklist();
-		return HM;
-	}
-	
-	@CrossOrigin
-	@RequestMapping(value = "employee/getWorkPicklist", method = RequestMethod.GET, produces = "application/json")
-	public @ResponseBody String getWorkPicklist() {
-		EmployeeManager employeemanager = (EmployeeManager) context.getBean("employeeManager");
-		String workPL = employeemanager.workMasterpicklist();
-		return workPL;
-	}
-			
-	
+		
 	@CrossOrigin
 	@RequestMapping(value = "empUpdate", method = RequestMethod.POST, consumes = { "multipart/form-data" })
 	public String empUpdate(@RequestParam("model") String jsondata, MultipartHttpServletRequest request) {	
@@ -461,41 +230,6 @@ public class EmployeeController {
 			e1.printStackTrace();
 		}
 		return "{\"response\":\"" + status.getMessage() + "\"}";
-	}
-		
-	private Status copyFiles(MultipartFile Files, String fileType,String uniqueid) {
-		String fileloc = fileLocation + File.separator + fileType;
-		File file = new File(fileloc);
-		if (file.exists()) {
-			OutputStream fos = null;
-			InputStream ios = null;
-			try {
-				ios = Files.getInputStream();
-				fos = new FileOutputStream(fileloc + File.separator + uniqueid+"_"+Files.getOriginalFilename());
-				int read = 0;
-				byte[] bytes = new byte[1024];
-
-				while ((read = ios.read(bytes)) != -1) {
-					fos.write(bytes, 0, read);
-				}
-				fos.flush();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				try {
-					fos.close();
-					ios.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		return new Status(true, "saved the file " + Files.getName());
 	}
 	
 	@CrossOrigin
@@ -606,22 +340,22 @@ public class EmployeeController {
 		}
 		return helpVid;
 	}
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// open/read the application context file
-		/*ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml");
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml");
 		DataSource datasource = (DataSource) ctx.getBean("dataSource");
 		EmployeeController employeeController = new EmployeeController();
 		String s = employeeController.getEmployee("123");
-		System.out.println(s);*/
+		System.out.println(s);
 		try {
-			 readExcel(0,2,33);
+			// readExcel(0,2,33);
 			// readNoClock();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}  
-	}
+	}*/
 	
 
 }
--- src/com/helius/dao/IEmployeeDAO.java
+++ src/com/helius/dao/IEmployeeDAO.java
@@ -26,30 +26,15 @@ import com.helius.entities.Help_Videos;
  */
 public interface IEmployeeDAO {
 	public Employee get(String employeeid);
-//	public void save(Employee employee,MultipartHttpServletRequest request,String offerId) throws Throwable;
-	public void delete(String employeeid);	
 	public ResponseEntity<byte[]> getEmployeeFiles(String employee_id,String filetype);	
 	public ResponseEntity<byte[]> getOfferFiles(String offerid,String filetype);	
-	public String masterpicklist();
-	public String getAllOffers();
 	public Employee getOfferDetails(String offerid);
 	public List<Employee_Offer_Details> getOfferDetailsByName(String offerName,String type);
-	//public String generateEmployeeId(String offerId);
-	public String WorkLocationPicklist();
-//	public String getAllEmployees();
-	public String getOpenOffers() throws Throwable;
 	public void empUpdate(Employee emp, MultipartHttpServletRequest request) throws Throwable;
 	public void offUpdate(Employee emp, MultipartHttpServletRequest request) throws Throwable;
-	public void saveJobOffer(Employee employee, MultipartHttpServletRequest request) throws Throwable;
-	public ArrayList<Object> getAllEmpIdName() throws Throwable;
 	public void sendEmailNotification(String jsonData) throws Throwable;
-//	public List<Object> getNewJoineeDashboard(String dateSelected,String fromdate,String thrudate) throws Throwable;
-	public List<Object> getAllNewEmployee(String dateSelected) throws Throwable;
-	public List<Object> getAllResignEmployee(String dateSelected,String fromdate,String thrudate) throws Throwable;
-//	public List<Object> getAllExitEmployee() throws Throwable;
 	ResponseEntity<byte[]> getHelpVideoFIle(int help_videos_id);
 	List<Help_Videos> getAllHelpVideos() throws Throwable;
 	public void saveOrUpdateHelpVideo(String help, MultipartHttpServletRequest request) throws Throwable;
 	public void deleteHelpVideo(String help_videos_id)throws Throwable;
-//	public String getOfferDashboard(String month);
 	}

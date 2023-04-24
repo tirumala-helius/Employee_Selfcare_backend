ae93d2582d3c3595d854b8c507efcb29590ae33f
khadar <82013273+nani2196@users.noreply.github.com>
2023-04-24 14:09:54 +0530

    HPITEST-4101 Backend implementation of Timesheet Automation in SSP

---
+++ src/com/helius/utils/OverTime.java
@@ -0,0 +1,31 @@
+package com.helius.utils;
+
+import java.sql.Timestamp;
+
+public class OverTime {
+
+	private Timestamp otDate;
+	private Timestamp otStarttime;
+	private Timestamp otEndtime;
+	public Timestamp getOtDate() {
+		return otDate;
+	}
+	public void setOtDate(Timestamp otDate) {
+		this.otDate = otDate;
+	}
+	public Timestamp getOtStarttime() {
+		return otStarttime;
+	}
+	public void setOtStarttime(Timestamp otStarttime) {
+		this.otStarttime = otStarttime;
+	}
+	public Timestamp getOtEndtime() {
+		return otEndtime;
+	}
+	public void setOtEndtime(Timestamp otEndtime) {
+		this.otEndtime = otEndtime;
+	}
+	
+	
+	
+}

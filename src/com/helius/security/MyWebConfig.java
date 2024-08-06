ec4e5699725411d716223b5249c82f476bb2cfb4
khadar <82013273+nani2196@users.noreply.github.com>
2024-08-06 17:15:20 +0530

    properties file chnages for SSP

--- src/com/helius/security/MyWebConfig.java
+++ src/com/helius/security/MyWebConfig.java
@@ -7,9 +7,11 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -23,7 +25,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.helius.security")
+@PropertySource("file:${jboss.server.home.dir}/conf/helius_hcm_ssp.properties")
 public class MyWebConfig extends WebMvcConfigurerAdapter{
+	
+	
+	 @Value("${mysql.jdbcUrl}") 
+	  private String dbUrl;
+	  
+	  @Value("${mysql.username}") 
+	  private String dbUsername;
+	  
+	  @Value("${mysql.password}")
+	  private String dbPassword;
+	  @Value("${mysql.driver}")
+	  private String driverClassName;
+	 
 
 	/**
 	 * 
@@ -42,12 +58,21 @@ public class MyWebConfig extends WebMvcConfigurerAdapter{
 
 	  @Bean
 	  public DataSource getDataSource() {
+			/*
+			 * BasicDataSource dataSource = new BasicDataSource();
+			 * dataSource.setDriverClassName(env.getProperty("mysql.driver"));
+			 * dataSource.setUrl(env.getProperty("mysql.jdbcUrl"));
+			 * dataSource.setUsername(env.getProperty("mysql.username"));
+			 * dataSource.setPassword(env.getProperty("mysql.password")); return dataSource;
+			 */
+	    
+	    
 	    BasicDataSource dataSource = new BasicDataSource();
-	    dataSource.setDriverClassName(env.getProperty("mysql.driver"));
-	    dataSource.setUrl(env.getProperty("mysql.jdbcUrl"));
-	    dataSource.setUsername(env.getProperty("mysql.username"));
-	    dataSource.setPassword(env.getProperty("mysql.password"));
-	    return dataSource;
+		  dataSource.setDriverClassName(driverClassName);
+		  dataSource.setUrl(dbUrl);
+		  dataSource.setUsername(dbUsername);
+		  dataSource.setPassword(dbPassword);
+	        return dataSource;
 	  }
 	  
 	  @Bean
